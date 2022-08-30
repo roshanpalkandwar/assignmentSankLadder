58eb364c12bd888ee0c0fd91a67f626e5f608888
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-30 19:16:38 +0530

    [roshan]welecome to sanke ladder game

---
+++ sanakeLadderAss/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sanakeLadderAss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welecome to snake ladder game");
+        }
+    }
+}
